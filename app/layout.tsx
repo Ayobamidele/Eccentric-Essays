362811f3cd681a96c6de91075dcc6340ce8e2164
Ayobamidele Ewetuga <ayobamideleewetuga@gmail.com>
2025-12-04 13:33:51 +0100

    feat: Integrate Tawk.to live chat script into RootLayout for enhanced user support

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type React from "react"
 import type { Metadata } from "next"
+import Script from "next/script"
 import { Inter, JetBrains_Mono } from "next/font/google"
 import { Analytics } from "@vercel/analytics/next"
 import "./globals.css"
@@ -34,6 +35,20 @@ export default function RootLayout({
           {children}
           <Analytics />
         </Providers>
+        <Script id="tawk-to" strategy="afterInteractive">
+          {`
+            var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+            (function () {
+              var s1 = document.createElement("script"),
+                s0 = document.getElementsByTagName("script")[0];
+              s1.async = true;
+              s1.src = "https://embed.tawk.to/693176bdf1aec6197f66e9bc/1jbkjfl5s";
+              s1.charset = "UTF-8";
+              s1.setAttribute("crossorigin", "*");
+              s0.parentNode.insertBefore(s1, s0);
+            })();
+          `}
+        </Script>
       </body>
     </html>
   )
